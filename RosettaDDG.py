9c0a08695b9d767a4eaf3b5ba58d0311f3fb1a3c
我的头很铁 <90082453+CQS2001@users.noreply.github.com>
2022-01-07 17:35:30 +0800

    Add files via upload

--- RosettaDDG.py
+++ RosettaDDG.py
@@ -1,23 +1,32 @@
-from rosetta import *
-rosetta.init()
+from pyrosetta import *
+from pyrosetta.rosetta import *
+from pyrosetta.toolbox import *
+init("-ignore_unrecognized_res 1 -ex1 -ex2 -flip_HNQ -relax:cartesian -nstruct 200 -crystal_refine -optimization:default_max_cycles 200")
+testPose= Pose()
+testPose = pose_from_pdb("1BNI.relax.pdb")
 
-def calc_ddG(pose,sfxn,resnum,aa):
-    native_score=sfxn(pose)
-    mutate_pose=mutate_residue(pose,resnum,aa,sfxn)
-    pack_rot=PackRotamersMover()
-    pack_rot.apply(pose)
+from pyrosetta.rosetta.protocols.relax import FastRelax
+scorefxn = pyrosetta.create_score_function("ref2015_cart")
+relax = pyrosetta.rosetta.protocols.relax.FastRelax()
+relax.constrain_relax_to_start_coords(True)
+relax.coord_constrain_sidechains(True)
+relax.ramp_down_constraints(False)
+relax.cartesian(True)
+#relax.min_type("dfpmin_armijo_nonmonotone")
+relax.min_type("lbfgs_armijo_nonmonotone")#for non-Cartesian scorefunctions use'dfpmin_armijo_nonmonotone'
+relax.set_scorefxn(scorefxn)
+s0=scorefxn(testPose)
+#relax.apply(testPose)
+s1=scorefxn(testPose)
+#testPose.dump_pdb('3GB1.relax.pdb')
+AA=['G','A','L','M','F','W','K','Q','E','S','P','V','I','C','Y','H','R','N','D','T']
+ddG=[]
+mp=Pose()
+for i in AA:
+    mp.assign(testPose)
+    mutate_residue(mp,52,i)
+    s2=scorefxn(mp)
+    dg=s2-s1
+    ddG.append(dg)
 
-    mutant_score=sfxn(mutated_pose)
-
-    return mutant_score - native_score
-
-
-pose = pose_from_pdb("3gp6.pdb")
-add_memb=AddMembraneMover()
-add_memb.apply(pose)
-init_mem_pos=MembranePositionFromTopology()
-init_mem_pos.apply(pose)
-
-sfxn=create_score_function("mpframework_fa_smooth_2014")
-ala_mut_ddG=calc_ddG(pose,sfxn,181,'A')
-print (ala_mut_ddG)
+print(ddG)
